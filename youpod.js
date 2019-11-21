4fa69e2bcdfc4ce5bad410a33b19ccfee49c0c30
Bigaston <bigaston1@gmail.com>
2019-11-21 12:13:58 +0100

    Téléchargement de l'audio

--- youpod.js
+++ youpod.js
@@ -3,13 +3,18 @@ const fs = require('fs');
 const path = require("path");
 const mustache = require("mustache");
 const Parser = require("rss-parser");
+const download = require('download');
+const { exec } = require('child_process');
  
 var convert = htmlConvert();
 var parser = new Parser();
+var feed;
 
 var template = fs.readFileSync(path.join(__dirname, "/template/default.mustache"), "utf8");
 
-parser.parseURL("https://point-games.lepodcast.fr/rss", (err, feed) => {
+parser.parseURL("https://script.lepodcast.fr/rss", (err, lFeed) => {
+  feed = lFeed
+
   var renderObj = {
     "imageURL": feed.image.url,
     "epTitle": feed.items[0].title,
@@ -20,7 +25,17 @@ parser.parseURL("https://point-games.lepodcast.fr/rss", (err, feed) => {
   var string = mustache.render(template, renderObj)
   fs.writeFileSync(path.join(__dirname, "tmp", "page.html"), string);
 
-  fs.createReadStream(path.join(__dirname, "tmp", "page.html"))
+  stream = fs.createReadStream(path.join(__dirname, "tmp", "page.html"))
     .pipe(convert())
     .pipe(fs.createWriteStream(path.join(__dirname, "tmp", "out.png")))
+    
+  stream.on("finish", downloadAudio);
 })
+
+function downloadAudio() {
+  console.log("Démarage du téléchargement")
+  download(feed.items[0].enclosure.url).then(data => {
+    fs.writeFileSync(path.join(__dirname, "/tmp/audio.mp3"), data);
+    console.log("Fichier téléchargé!");
+  });
+}
