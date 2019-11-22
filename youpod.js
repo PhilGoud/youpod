1da097fafe3b2dfc26dabc9936eff73b0fa3f823
Bigaston <bigaston1@gmail.com>
2019-11-22 17:09:17 +0100

    Envoit Email

--- youpod.js
+++ youpod.js
@@ -10,6 +10,15 @@ const config = require("./config.json")
 const bodyParser = require('body-parser');
 const randtoken = require('rand-token');
 const sq = require('sqlite3');
+const nodemailer = require("nodemailer");
+
+var transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		   user: config.mail,
+		   pass: config.password
+	   }
+});
 
 var app = express()
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -23,12 +32,11 @@ var db = new sq.Database(__dirname + "/base.db");
 
 //Reprise des générations en cas d'erreur
 flush();
+setInterval(flush, 1000 * 60 * 15);
 restartGeneration();
  
 var convert = htmlConvert();
 var parser = new Parser();
-
-var template = fs.readFileSync(path.join(__dirname, "/template/default.mustache"), "utf8");
 
 app.get("/static/:file", (req, res) => {
   res.sendFile(path.join(__dirname, "/web/static/", req.params.file))
@@ -47,10 +55,42 @@ app.get("/", (req, res) => {
   })
 })
 
+app.get("/download/:id", (req, res) => {
+  if (req.query.token != undefined) {
+    db.all(`SELECT * FROM video WHERE id='${req.params.id}'`, (err, rows) => {
+      if (rows.length >= 1) {
+        if (req.query.token != rows[0].access_token) {
+          res.status(403).send("Vous n'avez pas accès à cette vidéo")
+        } else {
+          if (rows[0].status == 'finished') {
+            res.download(path.join(__dirname, "/video/", `output_${rows[0].id}.mp4`))
+          } else if (rows[0].status == 'deleted') {
+            res.status(404).send("Cette vidéo à été supprimée du site!")
+          } else if (rows[0].status == 'during') {
+            res.status(404).send("Cette vidéo est encore en cours de traitement, revenez plus tard!")
+          } else {
+            res.status(404).send("Cette vidéo est encore dans la file d'attente.")
+          }
+        }
+      } else {
+        res.status(404).send("Cette vidéo n'est pas disponible...")
+      }
+    })
+  } else {
+    res.status(404).send("Vous n'avez pas mis de token d'accès à une vidéo")
+  }
+
+})
+
 app.post("/addvideo", (req, res) => {
-  db.run(`INSERT INTO video(email, rss, access_token) VALUES ("${req.body.email}", "${req.body.rss}", "${randtoken.generate(16)}")`)
-  initNewGeneration();
-  res.send("Vidéo correctement ajoutée à la liste!")
+  if (req.body.email != undefined && req.body.rss != undefined) {
+    db.run(`INSERT INTO video(email, rss, access_token) VALUES ("${req.body.email}", "${req.body.rss}", "${randtoken.generate(32)}")`)
+    initNewGeneration();
+    res.send("Vidéo correctement ajoutée à la liste!")
+  } else {
+    res.status(400).send("Votre requète n'est pas complète...")
+  }
+
 })
 
 // FONCTION DE GENERATIONS
@@ -66,7 +106,7 @@ function flush() {
     time = Date.now() - row.end_timestamp
     time = time / (1000 * 60 * 60);
 
-    if (time > 24) {
+    if (time > config.keeping_time) {
       fs.unlinkSync(path.join(__dirname, "/video/", `output_${row.id}.mp4`))
       db.run(`UPDATE video SET status='deleted' WHERE id=${row.id}`);
       console.log("Flush video " + row.id)
@@ -94,6 +134,8 @@ function generateFeed(feed_url, id) {
   parser.parseURL("http://glebeskefe.lepodcast.fr/rss", (err, lFeed) => {
     console.log(id + " Récupération du flux")
     feed = lFeed
+
+    var template = fs.readFileSync(path.join(__dirname, "/template/default.mustache"), "utf8");
 
     var renderObj = {
       "imageURL": feed.image.url,
@@ -137,10 +179,33 @@ function generateVideo(id) {
       fs.unlinkSync(path.join(__dirname, "/tmp/", `page_${id}.html`))
       fs.unlinkSync(path.join(__dirname, "/tmp/", `audio_${id}.mp3`))
 
+      sendMail(id);
       initNewGeneration();
     } else {
       console.log(err)
     }
+  })
+}
+
+function sendMail(id) {
+  db.all(`SELECT * FROM video WHERE id='${id}'`, (err, rows) => {
+    template = fs.readFileSync(path.join(__dirname, "/web/mail.mustache"), "utf8")
+    renderObj = {
+      "rss_link": rows[0].rss,
+      "keeping_time": config.keeping_time,
+      "video_link": config.host + "/download/" + id + "?token=" + rows[0].access_token
+    }
+
+    const mailOptions = {
+      from: 'youpod@balado.tools', // sender address
+      to: rows[0].email, // list of receivers
+      subject: `Vidéo générée sur Youpod!`, // Subject line
+      html: mustache.render(template, renderObj)
+    };
+    
+    transporter.sendMail(mailOptions, function (err, info) {
+      if(err) return console.log(err)
+    });
   })
 }
 
