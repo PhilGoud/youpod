ddcce3875451e5053ba51094392d4c238bcb5799
Bigaston <bigaston1@gmail.com>
2019-11-22 15:45:06 +0100

    Génération de vidéos

--- youpod.js
+++ youpod.js
@@ -8,16 +8,25 @@ const { exec } = require('child_process');
 const express = require('express')
 const config = require("./config.json")
 const bodyParser = require('body-parser');
+const randtoken = require('rand-token');
+const sq = require('sqlite3');
 
 var app = express()
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 })); 
+
+//Connexion à la base de donnée
+sq.verbose();
+var db = new sq.Database(__dirname + "/base.db");
+
+//Reprise des générations en cas d'erreur
+flush();
+restartGeneration();
  
 var convert = htmlConvert();
 var parser = new Parser();
-var feed;
 
 var template = fs.readFileSync(path.join(__dirname, "/template/default.mustache"), "utf8");
 
@@ -26,23 +35,64 @@ app.get("/static/:file", (req, res) => {
 })
 
 app.get("/", (req, res) => {
-  template = fs.readFileSync(path.join(__dirname, "/web/index.mustache"), "utf8")
+  db.all(`SELECT count(*) FROM video WHERE status='waiting'`, (err, rows) => {
+    template = fs.readFileSync(path.join(__dirname, "/web/index.mustache"), "utf8")
 
-  var render_object = {
-    "waiting_list": 0
-  }
-
-  res.setHeader("content-type", "text/html");
-  res.send(mustache.render(template, render_object))
+    var render_object = {
+      "waiting_list": rows[0]["count(*)"]
+    }
+  
+    res.setHeader("content-type", "text/html");
+    res.send(mustache.render(template, render_object))
+  })
 })
 
-app.post("")
+app.post("/addvideo", (req, res) => {
+  db.run(`INSERT INTO video(email, rss, access_token) VALUES ("${req.body.email}", "${req.body.rss}", "${randtoken.generate(16)}")`)
+  initNewGeneration();
+  res.send("Vidéo correctement ajoutée à la liste!")
+})
 
 // FONCTION DE GENERATIONS
-function generateFeed(feed_url) {
-  console.log("Démarage de la création")
+function restartGeneration() {
+  console.log("Reprise de générations...")
+  db.each(`SELECT * FROM video WHERE status='during'`, (err, row) => {
+    generateFeed(row.rss, row.id)
+  })
+}
+
+function flush() {
+  db.each(`SELECT * FROM video WHERE status='finished'`, (err, row) => {
+    time = Date.now() - row.end_timestamp
+    time = time / (1000 * 60 * 60);
+
+    if (time > 24) {
+      fs.unlinkSync(path.join(__dirname, "/video/", `output_${row.id}.mp4`))
+      db.run(`UPDATE video SET status='deleted' WHERE id=${row.id}`);
+      console.log("Flush video " + row.id)
+
+    }
+
+  })
+}
+
+function initNewGeneration() {
+  db.all(`SELECT count(*) FROM video WHERE status='during'`, (err, rows) => {
+    if (rows[0]["count(*)"] < config.max_during) {
+      db.all(`SELECT * FROM video WHERE status='waiting'`, (err, rows) => {
+        if(rows.length >= 1) {
+          db.run(`UPDATE video SET status='during' WHERE id=${rows[0].id}`);
+          generateFeed(rows[0].rss, rows[0].id)
+        }
+      })
+    }
+  })
+}
+
+function generateFeed(feed_url, id) {
+  console.log(id + " Démarage de la création")
   parser.parseURL("http://glebeskefe.lepodcast.fr/rss", (err, lFeed) => {
-    console.log("Récupération du flux")
+    console.log(id + " Récupération du flux")
     feed = lFeed
 
     var renderObj = {
@@ -53,33 +103,43 @@ function generateFeed(feed_url) {
     }
 
     var string = mustache.render(template, renderObj)
-    fs.writeFileSync(path.join(__dirname, "tmp", "page.html"), string);
+    fs.writeFileSync(path.join(__dirname, "tmp", `page_${id}.html`), string);
 
-    console.log("Génération de l'image");
+    console.log(id + " Génération de l'image");
 
-    stream = fs.createReadStream(path.join(__dirname, "tmp", "page.html"))
+    stream = fs.createReadStream(path.join(__dirname, "tmp", `page_${id}.html`))
       .pipe(convert())
-      .pipe(fs.createWriteStream(path.join(__dirname, "tmp", "overlay.png")))
+      .pipe(fs.createWriteStream(path.join(__dirname, "tmp", `overlay_${id}.png`)))
       
-    stream.on("finish", downloadAudio);
+    stream.on("finish", () => {
+      downloadAudio(id)
+    });
   })
 }
 
-function downloadAudio() {
-  console.log("Démarage du téléchargement")
+function downloadAudio(id) {
+  console.log(id + " Démarage du téléchargement")
   download(feed.items[0].enclosure.url).then(data => {
-    fs.writeFileSync(path.join(__dirname, "/tmp/audio.mp3"), data);
-    console.log("Fichier téléchargé!");
-    generateVideo();
+    fs.writeFileSync(path.join(__dirname, `/tmp/audio_${id}.mp3`), data);
+    console.log(id + " Fichier téléchargé!");
+    generateVideo(id);
   });
 }
 
-function generateVideo() {
-  console.log("Démarage de la génération de la vidéo")
+function generateVideo(id) {
+  console.log(id + " Démarage de la génération de la vidéo")
 
-  exec(`ffmpeg -i ./loop/loop.mp4 -i ./tmp/overlay.png -filter_complex "overlay=0:0" -i ./tmp/audio.mp3 -shortest -acodec copy ./tmp/output.mp4`, {cmd: __dirname}, (err, stdout, stderr) => {
+  exec(`ffmpeg -y -i ./loop/loop.mp4 -i ./tmp/overlay_${id}.png -filter_complex "overlay=0:0" -i ./tmp/audio_${id}.mp3 -shortest -acodec copy ./video/output_${id}.mp4`, {cmd: __dirname}, (err, stdout, stderr) => {
     if(err == undefined) {
-      console.log("Vidéo générée!")
+      console.log(id + " Vidéo générée!")
+      db.run(`UPDATE video SET status='finished', end_timestamp='${Date.now()}' WHERE id=${id}`);
+      fs.unlinkSync(path.join(__dirname, "/tmp/", `overlay_${id}.png`))
+      fs.unlinkSync(path.join(__dirname, "/tmp/", `page_${id}.html`))
+      fs.unlinkSync(path.join(__dirname, "/tmp/", `audio_${id}.mp3`))
+
+      initNewGeneration();
+    } else {
+      console.log(err)
     }
   })
 }
@@ -92,7 +152,7 @@ function generateLoop(duration) {
   }
 
   fs.writeFileSync(path.join(__dirname, "assets/list.txt"), string);
-  exec(`ffmpeg -f concat -i ./assets/list.txt ./loop/loop_${duration}.mp4`, {cmd: __dirname}, (err, stdout, stderr) => {
+  exec(`ffmpeg -y -f concat -i ./assets/list.txt ./loop/loop_${duration}.mp4`, {cmd: __dirname}, (err, stdout, stderr) => {
     console.log(err)
     console.log(stdout)
     console.log(stderr)
